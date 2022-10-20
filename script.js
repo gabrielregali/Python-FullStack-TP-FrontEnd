8ddca7e294a10107e3118463e30d8304f1559dd2
gabrielregali <88169218+gabrielregali@users.noreply.github.com>
2022-10-19 21:16:22 -0300

    Pagina Principal (main)

---
+++ script.js
@@ -0,0 +1,12 @@
+alert ("Bienvenidos a la pagina de Guillermo")
+
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("¡Se esperaba una dirección de correo electrónico!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
